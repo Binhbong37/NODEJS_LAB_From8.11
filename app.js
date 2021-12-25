21dcb0032cfcbdd2d80459e8506c030e96f91ead
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2021-12-25 13:11:40 +0700

    HẾT LAB 09

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
-const csrf = require('csurf')
+const csrf = require('csurf');
 const flash = require('connect-flash');
 
 const errorController = require('./controllers/error');
@@ -18,8 +18,7 @@ const store = new MongoDBStore({
   uri: MONGODB_URI,
   collection: 'sessions'
 });
-const csrfProtection = csrf()
-app.use(flash());
+const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -38,36 +37,57 @@ app.use(
     store: store
   })
 );
-app.use(csrfProtection)
+app.use(csrfProtection);
+app.use(flash());
 
 app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+app.use((req, res, next) => {
+  // throw new Error('Sync Dummy');
   if (!req.session.user) {
     return next();
   }
   User.findById(req.session.user._id)
     .then(user => {
+      if (!user) {
+        return next();
+      }
       req.user = user;
       next();
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      next(new Error(err));
+    });
 });
-
-app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.session.isLoggedIn;
-  res.locals.csrfToken = req.csrfToken();
-  next()
-})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
+app.get('/500', errorController.get500);
+
 app.use(errorController.get404);
+
+app.use((error, req, res, next) => {
+  // res.status(error.httpStatusCode).render(...);
+  // res.redirect('/500');
+  res.status(500).render('500', {
+    pageTitle: 'Error!',
+    path: '/500',
+    isAuthenticated: req.session.isLoggedIn
+  });
+});
 
 mongoose
   .connect(MONGODB_URI)
-  .then(() => {
-    app.listen(3000)
-    console.log("Da ket noi voi MongOOSE Lab !!!")
+  .then(result => {
+    app.listen(3000);
+    console.log("Da ket noi vs MONGOOSE")
   })
-  .catch(err => console.log("Ket noi vs Mongoose That bai"))
+  .catch(err => {
+    console.log(err);
+  });
