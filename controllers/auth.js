01f06e0aa2cabaae0d67c4eedeae5dc878b7d677
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2021-12-25 08:32:24 +0700

    Lab 8.15, 8.16: Mã hóa mật khẩu

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,7 @@
 const User = require('../models/user');
 
+const bcrypt = require('bcryptjs')
+
 exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
     path: '/login',
@@ -39,9 +41,12 @@ exports.postSignup = (req, res, next) => {
     if(userDoc) {
       return res.redirect("/signup")
     }
+    return bcrypt.hash( password, 12)
+  })
+  .then(hashPass => {
     const newUser = new User({
       email: email,
-      password: password,
+      password: hashPass,
       cart: { items: [] }
     })
     return newUser.save()
