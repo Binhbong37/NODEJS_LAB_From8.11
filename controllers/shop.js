514ba932a059dd55e53aa1afac006ef104534f98
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2021-12-25 16:13:25 +0700

    Lab 10.10: Đặt tiêu đề loại tệp

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,3 +1,6 @@
+const fs = require('fs')
+const path = require('path')
+
 const Product = require('../models/product');
 const Order = require('../models/order');
 
@@ -146,3 +149,19 @@ exports.getOrders = (req, res, next) => {
       return next(error);
     });
 };
+
+exports.getInvoice = (req, res, next) => {
+  const orderId = req.params.orderId;
+  const invoiceName = 'invoice-' + orderId +'.pdf';
+  const invoicePath = path.join('data', 'invoices', invoiceName)
+  fs.readFile(invoicePath, (err, data) => {
+    if(err) {
+      return next(err)
+    }
+    res.setHeader('content-Type', 'application/pdf')
+    res.setHeader('content-Disposition', 'inline; filename="' + invoiceName + '"')
+    // res.setHeader('content-Disposition', 'attached; filename="' + invoiceName + '"')
+    res.send(data)
+  })
+
+}
