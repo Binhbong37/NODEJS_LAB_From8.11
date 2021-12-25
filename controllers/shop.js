4a5981a6b6215ee490eb96482ce7e907a7d6c4ba
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2021-12-26 06:59:16 +0700

    Lab 10.13: Sử dụng PDFKit để tạo .pdf

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const path = require('path')
+const PDFDocument = require('pdfkit')
 
 const Product = require('../models/product');
 const Order = require('../models/order');
@@ -161,7 +162,20 @@ exports.getInvoice = (req, res, next) => {
       return next(new Error('Khong dung nguoi dung !'))
     }
     const invoiceName = 'invoice-' + orderId +'.pdf';
-    const invoicePath = path.join('data', 'invoices', invoiceName)
+    const invoicePath = path.join('data', 'invoices', invoiceName);
+    const pdfDoc = new PDFDocument();
+    res.setHeader('content-Type', 'application/pdf')
+    res.setHeader(
+      'content-Disposition',
+      'inline; filename="' + invoiceName + '"'
+    );
+
+    pdfDoc.pipe(fs.createWriteStream(invoicePath));
+    pdfDoc.pipe(res);
+
+    pdfDoc.text('Hello World')
+
+    pdfDoc.end()
     // fs.readFile(invoicePath, (err, data) => {
     //   if(err) {
     //     return next(err)
@@ -171,10 +185,10 @@ exports.getInvoice = (req, res, next) => {
     //   // res.setHeader('content-Disposition', 'attached; filename="' + invoiceName + '"')
     //   res.send(data)
     // })
-    const file = fs.createReadStream(invoicePath);
-    res.setHeader('content-Type', 'application/pdf')
-    res.setHeader('content-Disposition', 'inline; filename="' + invoiceName + '"')
-    file.pipe(res)
+    // const file = fs.createReadStream(invoicePath);
+    // res.setHeader('content-Type', 'application/pdf')
+    // res.setHeader('content-Disposition', 'inline; filename="' + invoiceName + '"')
+    // file.pipe(res)
   
   })
   .catch(err => console.log(err))
